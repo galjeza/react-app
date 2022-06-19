552acd9f059dadd6ad62e8726b40081e15884af9
galjeza <gal.jeza@protonmail.com>
2022-06-19 13:13:28 +0200

    Added expenses.js
    Contains hardcoded expense array of objects

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import ExpenseItem from "./components/ExpenseItem";
-
-
+import expenses from "./expenses";
 
 function App() {
   return (
     <div>
-      <ExpenseItem></ExpenseItem>
+      {expenses.map((expense) => {
+        return (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            price={expense.amount}
+            date={expense.date}
+          ></ExpenseItem>
+        );
+      })}
     </div>
   );
 }
